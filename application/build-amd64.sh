769880dae014d8af3b85e036426398e8ce72a0c7
Evgeniy <37136002+PetrovEvgeniy@users.noreply.github.com>
2024-11-26 19:45:23 +0100

    Update build-amd64.sh

--- application/build-amd64.sh
+++ application/build-amd64.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Build a snap of the whole battery diagnosis app
+# Build a snap
 snapcraft clean --destructive-mode
 snapcraft --destructive-mode
 snapcraft clean --destructive-mode
@@ -8,7 +8,5 @@ snapcraft clean --destructive-mode
 # Remove the build directory
 rm -rf ./build
 
-#TODO: Remove before push
-mv *.snap /media/sf_shared/18.11.2024
 
 
